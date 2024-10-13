522d1fcac32b5d2bad067ed0c93f09aacaed9659
Pattarakorn Kuncharoen <pattarakorn.kunc@kmutt.ac.th>
2024-10-13 21:00:17 +0700

    search

--- routes/myRoute.js
+++ routes/myRoute.js
@@ -1,9 +1,9 @@
 import express, { query } from "express";
 import db from "../config/database.js";
-import multer from "multer"; 
-import { MongoClient, Binary } from 'mongodb';
-import fs from 'fs';
-import zlib from 'zlib'
+// import multer from "multer"; 
+// import { MongoClient, Binary } from 'mongodb';
+// import fs from 'fs';
+// import zlib from 'zlib'
 
 const router = express.Router()
 const ticker = db.collection('stock_ticker');
@@ -34,6 +34,28 @@ const ticker = db.collection('stock_ticker');
     } catch (error) {
       console.error("Error:", error);
       res.status(500).json({ error: "Internal Server Error" });
+    }
+  })
+
+  router.get("/searchFromTic/:identifier", async (req, res) => {
+    try {
+  
+      const identifier = req.params.identifier;
+      const formForShow = { projection: { _id: 0, ticker: 1, name: 1, market: 1, type: 1 } }
+      const solution = {
+        $or: [
+          { ticker: { $regex: `^${identifier}`, $options: 'i' } },
+          { name: { $regex: `^${identifier}`, $options: 'i' } }
+        ]
+      }; 
+      const ResultTic = await ticker.find(
+        solution, formForShow
+      ).toArray();
+  
+      res.status(200).json(ResultTic); // ส่งผลลัพธ์กลับ
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal Server Error" }); // จัดการข้อผิดพลาด
     }
   })
 
